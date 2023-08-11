d1ace2a110435d624bc1d340010be2f09c423b6d
ctcticxh <3574736597@qq.com>
2023-08-11 10:48:36 +0800

    add

---
+++ plugin/garbage_classification/main.go
@@ -0,0 +1,41 @@
+// 垃圾分类
+package garbageclassification
+
+import (
+	"fmt"
+
+	"github.com/FloatTech/floatbox/web"
+	ctrl "github.com/FloatTech/zbpctrl"
+	"github.com/FloatTech/zbputils/control"
+	"github.com/tidwall/gjson"
+	zero "github.com/wdvxdr1123/ZeroBot"
+	"github.com/wdvxdr1123/ZeroBot/message"
+)
+
+const url = "https://api.a20safe.com/api.php?api=49&key=471548b54e6625f0a4915a390a9bd5db&word=%v"
+
+func init() {
+	engine := control.Register("garbageclassification", &ctrl.Options[*zero.Ctx]{
+		DisableOnDefault: false,
+		Brief:            "垃圾分类",
+		Help:             "xxx是什么垃圾\n",
+	})
+	engine.OnFullMatch("疯狂星期四").SetBlock(true).Handle(func(ctx *zero.Ctx) {
+		data, err := web.GetData(url)
+		if err != nil {
+			ctx.SendChain(message.Text("ERROR: ", err))
+			return
+		}
+		ctx.SendChain(message.Text(gjson.ParseBytes(data).Get("data.#.result").Array()[0]))
+
+	})
+	engine.OnSuffix("是什么垃圾").Handle(func(ctx *zero.Ctx) {
+		args := ctx.State["args"].(string)
+		data, err := web.GetData(fmt.Sprintf(url, args))
+		if err != nil {
+			ctx.SendChain(message.Text("ERROR: ", err))
+			return
+		}
+		ctx.SendChain(message.Text(args, "是", gjson.ParseBytes(data).Get("data.#.result").Array()[0]))
+	})
+}
