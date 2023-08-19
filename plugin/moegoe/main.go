9484371d6265dbb28e911af9ccdf9c132667d4ee
ctcticxh <3574736597@qq.com>
2023-08-19 23:41:21 +0800

    add

--- plugin/moegoe/main.go
+++ plugin/moegoe/main.go
@@ -17,6 +17,8 @@ import (
 const (
 	jpapi = "https://moegoe.azurewebsites.net/api/speak?text=%s&id=%d"
 	krapi = "https://moegoe.azurewebsites.net/api/speakkr?text=%s&id=%d"
+	/*jpapi = "127.0.0.1:8000/%s?text=%s"
+	krapi = "127.0.0.1:8000/%s?text=%s"*/
 )
 
 var speakers = map[string]uint{
@@ -35,6 +37,7 @@ func init() {
 			"- 让[Sua|Mimiru|Arin|Yeonhwa|Yuhwa|Seonbae]说(韩语)\n" +
 			"- 让[派蒙|凯亚|安柏|丽莎|琴|香菱|枫原万叶|迪卢克|温迪|可莉|早柚|托马|芭芭拉|优菈|云堇|钟离|魈|凝光|雷电将军|北斗|甘雨|七七|刻晴|神里绫华|雷泽|神里绫人|罗莎莉亚|阿贝多|八重神子|宵宫|荒泷一斗|九条裟罗|夜兰|珊瑚宫心海|五郎|达达利亚|莫娜|班尼特|申鹤|行秋|烟绯|久岐忍|辛焱|砂糖|胡桃|重云|菲谢尔|诺艾尔|迪奥娜|鹿野院平藏]说(中文)",
 	}).ApplySingle(ctxext.DefaultSingle)
+
 	en.OnRegex("^让(宁宁|爱瑠|芳乃|茉子|丛雨|小春|七海)说([A-Za-z\\s\\d\u3005\u3040-\u30ff\u4e00-\u9fff\uff11-\uff19\uff21-\uff3a\uff41-\uff5a\uff66-\uff9d\\pP]+)$").Limit(ctxext.LimitByGroup).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
 			text := ctx.State["regex_matched"].([]string)[2]
@@ -49,11 +52,21 @@ func init() {
 		})
 	en.OnRegex("^让(派蒙|凯亚|安柏|丽莎|琴|香菱|枫原万叶|迪卢克|温迪|可莉|早柚|托马|芭芭拉|优菈|云堇|钟离|魈|凝光|雷电将军|北斗|甘雨|七七|刻晴|神里绫华|雷泽|神里绫人|罗莎莉亚|阿贝多|八重神子|宵宫|荒泷一斗|九条裟罗|夜兰|珊瑚宫心海|五郎|达达利亚|莫娜|班尼特|申鹤|行秋|烟绯|久岐忍|辛焱|砂糖|胡桃|重云|菲谢尔|诺艾尔|迪奥娜|鹿野院平藏)说([\\s\u4e00-\u9fa5\\pP]+)$").Limit(ctxext.LimitByGroup).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
+			fmt.Println("触发了1")
 			if 原.k == "" {
+				fmt.Println("触发了2")
 				return
 			}
+			fmt.Println("触发了")
 			text := ctx.State["regex_matched"].([]string)[2]
 			id := speakers[ctx.State["regex_matched"].([]string)[1]]
 			ctx.SendChain(message.Record(fmt.Sprintf(genshin.CNAPI, id, url.QueryEscape(text), url.QueryEscape(原.k))))
 		})
+
+	/*en.OnRegex(`^让(.*)说(.*)$`).Limit(ctxext.LimitByGroup).SetBlock(true).
+	Handle(func(ctx *zero.Ctx) {
+		text := ctx.State["regex_matched"].([]string)[2]
+		name := ctx.State["regex_matched"].([]string)[1]
+		ctx.SendChain(message.Record(fmt.Sprintf(jpapi, name, text)))
+	})*/
 }
